d40396ec0797592f769484b63d62e26dac727001
= <=>
2020-07-19 03:23:00 -0400

    Earthquakes map

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,13 +18,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
     ['Streets']: streets,
-    ['Satellite Streets']: satelliteStreets    
+    ['Satellite']: satelliteStreets    
   };
 
   // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [43.7, -79.3],
-	zoom: 11,
+	center: [39.5, -98.5],
+	zoom: 3,
 	layers: [streets]
 })
 
@@ -32,25 +32,25 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/IZT-GM/Mapping_Earthquakes/master/torontoNeighborhoods.json";
+// let torontoHoods = "https://raw.githubusercontent.com/IZT-GM/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-//   L.geoJson(data).addTo(map);
-  L.geoJson(data, {
-	// We turn each feature into a marker on the map.
-    style: myStyle,
-    onEachFeature: function(feature, layer) {
-		// console.log(layer);
-		layer.bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
-        }}).addTo(map);
+  L.geoJson(data).addTo(map);
+  // L.geoJson(data, {
+	// // We turn each feature into a marker on the map.
+  //   style: myStyle,
+  //   onEachFeature: function(feature, layer) {
+	// 	// console.log(layer);
+	// 	layer.bindPopup("<h2>Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
+  //       }}).addTo(map);
 });
 
-var myStyle = {
-  "color": "blue",
-  "weight": 1,
-  "fillColor"	: 'yellow',
-  "fillOpacity": 0.2
-};
+// var myStyle = {
+//   "color": "blue",
+//   "weight": 1,
+//   "fillColor"	: 'yellow',
+//   "fillOpacity": 0.2
+// };
